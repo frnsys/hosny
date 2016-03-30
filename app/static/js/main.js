d7798a22abbe3a33fcf2a1ec986d3137826c996a
binaricorn <fei.fei2liu@gmail.com>
2016-03-30 17:38:01 -0400

    graphs to main page

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -43,33 +43,67 @@ require([
   });
 
   $(function() {
+
+          var graphs = {
+            // mean_wage: new Graph(".graphs", "mean_wage", 650, 200, "mean wage"),
+            // mean_equip_price: new Graph(".graphs", "mean_equip_price", 650, 200, "mean equip price"),
+            // mean_material_price: new Graph(".graphs", "mean_material_price", 650, 200, "mean material price"),
+            // mean_consumer_good_price: new Graph(".graphs", "mean_consumer_good_price", 650, 200, "mean consumer good price"),
+            // mean_healthcare_price: new Graph(".graphs", "mean_healthcare_price", 650, 200, "mean healthcare price"),
+            // mean_equip_profit: new Graph(".graphs", "mean_equip_profit", 650, 200, "mean equip profit"),
+            // mean_material_profit: new Graph(".graphs", "mean_material_profit", 650, 200, "mean material profit"),
+            // mean_consumer_good_profit: new Graph(".graphs", "mean_consumer_good_profit", 650, 200, "mean consumer good profit"),
+            // mean_healthcare_profit: new Graph(".graphs", "mean_healthcare_profit", 650, 200, "mean healthcare profit"),
+            mean_quality_of_life: new Graph(".graphs-qli", "mean_quality_of_life", 650, 200, "mean quality of life"),
+            // mean_cash: new Graph(".graphs", "mean_cash", 650, 200, "mean cash"),
+            // n_sick: new Graph(".graphs", "n_sick", 650, 200, "n sick"),
+            // n_deaths: new Graph(".graphs", "n_deaths", 650, 200, "n deaths"),
+            // n_population: new Graph(".graphs", "n_population", 650, 200, "n population"),
+            // n_firms: new Graph(".graphs", "n_firms", 650, 200, "n firms"),
+            // n_bankruptcies: new Graph(".graphs", "n_bankruptcies", 650, 200, "n bankruptcies"),
+            // welfare: new Graph(".graphs", "welfare", 650, 200, "welfare"),
+            // tax_rate: new Graph(".graphs", "tax_rate", 650, 200, "tax rate")
+          };
+
+      socket.on("graph", function(data){
+        if (data.graph in graphs) {
+          var graph = graphs[data.graph];
+          graph.update(data.data);
+        }
+      });
       // have form already, next submits that form
       $(".next").on("click", function(ev) {
         ev.preventDefault();
 
-        var consumer_good_utility, labor_per_equipment, sickness_severity, transmission_rate;
+        var consumer_good_utility, consumer_good_utility_translation, labor_per_equipment, labor_per_equipment_translation, sickness_severity, sickness_severity_translation, transmission_rate, transmission_rate_translation;
 
         switch ($('[name=good_utility]:checked').val()) {
           case "1":
             consumer_good_utility = 10;
+            consumer_good_utility_translation = "Outstanding";
             break;
           case "2":
             consumer_good_utility = 1;
+            consumer_good_utility_translation = "Average";
             break;
           case "3":
             consumer_good_utility = 0.2;
+            consumer_good_utility_translation = "Poor";
             break;
         }
 
         switch ($('[name=per_equipment]:checked').val()) {
           case "1":
             labor_per_equipment = 200;
+            labor_per_equipment_translation = "Extremely automated";
             break;
           case "2":
             labor_per_equipment = 50;
+            labor_per_equipment_translation = "As it is now";
             break;
           case "3":
             labor_per_equipment = 5;
+            labor_per_equipment_translation = "Labor intensive";
             break;
         }
 
@@ -108,6 +142,9 @@ require([
             $('.overlay').fadeOut();
             $('.omni').fadeIn();
             $('.step-simulation').show();
+            console.log("hey");
+            $('.city-equation .consumer_good_utility').empty().text(consumer_good_utility + " " + consumer_good_utility_translation);
+            $('.city-equation .labor_per_equipment').empty().text(labor_per_equipment + " " + labor_per_equipment_translation);
           }
         });
 
