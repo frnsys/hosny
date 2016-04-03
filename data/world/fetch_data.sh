1b6b1657cfc74850a0e543654498eff793adf35f
Francis Tseng <f+accounts@frnsys.com>
2016-04-03 14:24:51 -0400

    fetch data note

--- data/world/fetch_data.sh
+++ data/world/fetch_data.sh
@@ -4,6 +4,7 @@ mkdir src
 
 # https://labor.ny.gov/stats/laus.asp
 wget "https://labor.ny.gov/stats/lausCSV.asp?PASS=1&geog=21093561" -O src/unemployment.csv
+echo "delete everything under and including the LABOR FORCE line"
 
 # http://data.okfn.org/data/core/s-and-p-500
 wget "http://data.okfn.org/data/core/s-and-p-500/r/data.csv" -O src/sp500.csv
